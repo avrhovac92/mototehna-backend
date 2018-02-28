6816109390f956931a0eeaf9fa51c72e9b84cb50
Aleksandar Vrhovac <avrhovac92@gmail.com>
2018-02-28 07:56:34 +0100

    Added Error handling, added routers, created middleware

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+
+// Routes which sould handle requests
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'it works'
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
   });
 });
 
