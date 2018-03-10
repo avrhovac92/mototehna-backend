cfaca520c9b405f4f9ea620b1a50c7c466270711
Aleksandar Vrhovac <avrhovac92@gmail.com>
2018-03-10 16:25:24 +0100

    Updated products section

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,43 +1,71 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 
+const Product = require('../models/product');
+
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /api/products'
-  });
+  Product.find()
+    .exec()
+    .then(docs => res.status(200).json(docs))
+    .catch(err => res.status(500).json({ message: err.message }));
 });
 
 router.post('/', (req, res, next) => {
-  res.status(201).json({
-    message: 'Handling POST requests to /api/products'
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
   });
+  product
+    .save()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json({ message: err.message }));
 });
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  if (id === 'special') {
-    res.status(200).json({
-      message: 'You discovered special ID',
-      id: id
-    });
-  } else {
-    res.status(200).json({
-      messsage: 'You passed an ID',
-      id: id
-    });
-  }
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'There is not product with id: ' + id });
+      }
+    })
+    .catch(err => res.status(500).json({ message: err.message }));
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated product'
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const key in req.body) {
+    updateOps[key] = req.body[key];
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result =>
+      res.status(200).json({ message: 'Product updated: ' + id, result })
+    )
+    .catch(err => res.status(500).json({ message: err.message }));
 });
 
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted product'
-  });
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result =>
+      res
+        .status(200)
+        .json({
+          message: 'Deleted product: ' + id,
+          result
+        })
+        .catch(err => res.status(500).json({ message: err.message }))
+    );
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect('mongodb://127.0.0.1:27017/mototehna', {});
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Routes which sould handle requests
 app.use('/api/products', productRoutes);
